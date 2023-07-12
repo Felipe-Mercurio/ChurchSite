35a3db593037bac5bfb2cf27b07e8617dd284f1b
Felipe Mercurio <mercuriofelipe10@gmail.com>
2023-07-12 19:27:19 -0300

    First page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
-import Header from "../components/header"
-import Main from "../components/main"
-import styles from "./Home.module.scss"
+import Header from "Projetos Next Js/ChurchSite/components/header"
+import Main from "Projetos Next Js/ChurchSite/components/components/main"
+import styles from "Projetos Next Js/ChurchSite/pages/Home.module.scss"
 import "/Projetos Next Js/ChurchSite/styles/globais.scss"
 
 function Home(){
