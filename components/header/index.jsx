896f094da95e06e65c316582610fc95cc22ffc3c
Felipe Mercurio <mercuriofelipe10@gmail.com>
2023-07-12 22:17:09 -0300

    First page

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Logo from "/public/images/logo.png";
+import Logo from "/public/images/logowhiteversion.png";
 import Styles from "./header.module.scss";
 
 const Header = () => {
--- components/main/index.jsx
+++ components/main/index.jsx
@@ -1,12 +1,10 @@
 import Image from "next/image";
+import styles from "./main.module.scss";
 import Logo from "/public/images/logo.png";
-import Styles from "./main.module.scss";
 
 const Main = () => {
-    return <div className={Styles.container}>
-        <div >
-            <Image src={Logo}/>
-        </div>
+    return <div className={styles.container}>
+        <Image src={Logo}/>
     </div>;
 }
 
