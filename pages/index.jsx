44a32d207ab25c683a6f6af416ad18f94a905df9
Felipe Mercurio <mercuriofelipe10@gmail.com>
2023-07-12 21:07:31 -0300

    First page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,6 @@
 import Header from "../components/header"
 import Main from "../components/main"
 import styles from "./Home.module.scss"
-import "/Projetos Next Js/ChurchSite/styles/globais.scss"
 
 function Home(){
     return <div className={styles.container}>
