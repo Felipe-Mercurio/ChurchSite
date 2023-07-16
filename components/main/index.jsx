f374ee0114a2bfdf8ff319df0b9de6c2de68074d
Felipe Mercurio <mercuriofelipe10@gmail.com>
2023-07-16 13:55:39 -0300

    First page

--- components/main/index.jsx
+++ components/main/index.jsx
@@ -1,10 +1,16 @@
-import Image from "next/image";
 import styles from "./main.module.scss";
-import Logo from "/public/images/logo.png";
 
 const Main = () => {
     return <div className={styles.container}>
-        <Image src={Logo}/>
+
+        <div className={styles.sobre}>
+            Sobre nós:
+            <br></br>
+            Igreja Fundada no ano de 1994 em cultos nos lares e evangelizações pelo Pastor Rivaldi da Silva Araujo, onde teve como sede a própria garagem e hoje com a ajuda de Deus tem sua sede na Rua Carioba, 44 Jd. Mirante Várzea Paulista. Em todo esse tempo enfrentou dificuldades por causa do evangelho de Deus e também por pregar a Unicidade ( Único Deus jesus ). Hoje o trabalho continua pela família do Pastor já falecido.
+            Temos o intuito de salvar almas, libertar pessoas através da verdade e que nosso descanso seja no Céu junto ao Senhor, suficiente Salvador e único Deus Jesus Cristo.
+        </div>
+
+
     </div>;
 }
 
