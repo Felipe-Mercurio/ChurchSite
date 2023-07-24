305460d9be8e6983d4fe028e26aed17d3b89c58d
Felipe Mercurio <mercuriofelipe10@gmail.com>
2023-07-24 01:42:51 -0300

    First page

--- components/main/index.jsx
+++ components/main/index.jsx
@@ -1,13 +1,13 @@
 import styles from "./main.module.scss";
+import { BsFillXDiamondFill } from 'react-icons/bs';
+
 
 const Main = () => {
     return <div className={styles.container}>
-
-        <div >
-            
+        <div className={styles.content}>
+            <div className={styles.diamondicon}>
+            </div>  
         </div>
-
-
     </div>;
 }
 
